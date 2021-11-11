8d3706ecdbb273aa128a1d9fbabca08609cfbaa3
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-11 14:16:52 +0200

    Ex3.2 done

--- Ex3.1-Growth_Of_population.js
+++ Ex3.1-Growth_Of_population.js
@@ -8,4 +8,4 @@ let nb_year = (p0, percent, aug, p) => {
 }
 
 console.log(nb_year(1500, 5, 100, 5000))   // -> 15
-console.log(nb_year(1500000, 2.5, 10000, 2000000))   // -> 10
+console.log(nb_year(1500000, 2.5, 10000, 2000000))   // -> 10s
