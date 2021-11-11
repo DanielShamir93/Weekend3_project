4f499dceefebc6968f9839b59a21162d3d56594a
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-11 13:16:41 +0200

    Ex2.3 done

---
+++ Ex2.3-Find_the_Next_Perfect_square.js
@@ -0,0 +1,9 @@
+const findNextSquare = num => {
+    let sqrt = Math.sqrt(num);
+    return Math.round(sqrt) === sqrt ? Math.pow(num+1, 2) : -1; 
+}
+
+
+console.log(findNextSquare(121)) // --> returns 144
+console.log(findNextSquare(625)) // --> returns 676
+console.log(findNextSquare(114)) // --> returns -1 since 114 is not a perfect
