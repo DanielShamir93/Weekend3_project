a96170be986328a5f55587ce05393279128b544e
DanielShamir93 <daniel.shamir.93@gmail.com>
2021-11-11 12:50:37 +0200

    Ex2 done

---
+++ Ex2.1-Sum_of_lowest_numbers.js
@@ -0,0 +1,13 @@
+const sumOf2LowestNums = arr => {
+    return arr.sort(compareNumbers).slice(0, 2).reduce(addNumbers);
+}
+
+const compareNumbers = (a, b) => {
+    return a - b;
+}
+
+const addNumbers = (a, b) => {
+    return a + b;
+}
+
+console.log(sumOf2LowestNums([19, 5, 42, 2, 77]));
